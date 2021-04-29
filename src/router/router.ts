57be6d378a9537d8df0c3f899ae658f6a195f804
YUZEJIA <755596386@qq.com>
2021-04-29 12:25:29 +0800

    webpack 配置修改

---
+++ src/router/router.ts
@@ -0,0 +1,14 @@
+export const routers = [
+    './html/drag.html'
+]
+const app = document.getElementById('app');
+
+class Router {
+
+    constructor() {
+
+    }
+
+
+}
+console.log('22233322222222');
